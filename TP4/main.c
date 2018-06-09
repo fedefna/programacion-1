033bb845b9385edfbfd3a000259ee0f8d7159708
fedefna <fede.fna@hotmail.com>
2018-06-08 22:22:49 -0300

    Add files via upload

---
+++ TP4/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    while(seguir=='s'){
+        system("cls");
+        printf("\n1- Parse del archivo data.csv\n");
+        printf("2- Listar Empleados\n");
+        printf("3- Ordenar por nombre\n");
+        printf("4- Agregar un elemento\n\n");
+        printf("5- Elimina un elemento\n");
+        printf("6- Listar Empleados (Desde/Hasta)\n");
+        printf("7- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                if(parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)){
+                    printf("\nSe parseo el archivo data.csv.\n");
+                }else{
+                    printf("\nError.\n");
+                }
+                system("pause");
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
---
+++ TP4/parser.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    char buffer[4][50];
+    pFile = fopen("data.csv", "r");
+
+    if(pFile == NULL)
+    {
+        printf("Error al abrir el fichero\n");
+        exit(1);
+    }
+
+    while(!feof(pFile))
+    {
+        cant = fscanf(pFile, "%[^,], %[^,], %[^,], %[^\n] \n", buffer[0], buffer[1], buffer[2], buffer[3]);
+
+        if( cant != 4)
+        {
+            cant = fscanf(pFile, "%[^,], %[^,], %[^,], %[^\n] \n", buffer[0], buffer[1], buffer[2], buffer[3]);
+
+            if(feof(f))
+            {
+                break;
+            }
+            else
+            {
+                printf("Problema para leer el archivo\n");
+
+                break;
+            }
+        }
+        id = atoi(idCad);
+        anio = atoi(anioCad);
+
+        auto1.id = id;
+        auto1.anio = anio;
+        strcpy(auto1.marca, marca);
+        strcpy(auto1.modelo, modelo);
+        strcpy(auto1.color, color);
+
+
+
+        printf("n1=%d n2=%s n3=%s n=%d modelo:%s\n",  auto1.id,  auto1.marca,  auto1.color,  auto1.anio,  auto1.modelo);
+
+    }
+
+    printf("\n\n");
+
+    fclose(f);
+    return 0;
+}
